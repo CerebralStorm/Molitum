dfb2cca86ebf944718ff7e04466283f9d9b62d65
Cody Tanner <CerebralStorm@gmail.com>
2012-09-12 08:51:48 -0600

    reverted asset precompile in production.rb to see if it will give same heroku error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,6 @@ Molitum::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  config.assets.precompile = [/^[-_a-zA-Z0-9]*\..*/]
-
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
